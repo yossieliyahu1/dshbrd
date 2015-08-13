8711302c17cc261b68a2c2e5e581c406a9d4d940
yossieliyahu <yossi@montiera.com>
2015-08-13 17:57:48 +0300

    a

---
+++ js/main.js
@@ -0,0 +1,15 @@
+﻿
+(function () {
+	$(".dataTable_wrapper").html('<table class="table table-striped table-bordered table-hover" id="users-table"></table>');
+	$.getJSON("http://localhost:3221/userlist/?v=1&callback=?", function (data) {
+		$('#users-table').DataTable({
+			"data": data,
+			"columns": [
+				{ "title": "User ID", "defaultContent" : "", "data" : "_id" },
+				{ "title": "Country", "defaultContent": "", "data": "ct" },
+				{ "title": "Product", "defaultContent": "", "data": "pr" },
+				{ "title": "Data", "defaultContent": "", "data": "data" }
+			]
+		});
+	});
+})();
